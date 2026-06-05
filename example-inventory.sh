d7fe3f67f6f97f10b956e44192e2c56a69689e1e
Evgenii Buchnev <40753176+evgeniibuchnev@users.noreply.github.com>
2026-06-05 13:07:38 +1000

    example-inventory.sh - comment added

--- example-inventory.sh
+++ example-inventory.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# Example inventory script: outputs a newline-separated list of hosts for use with shell-menu
 
 LIST="webserver01 webserver02 webserver03"
 for HOST in $LIST; do
